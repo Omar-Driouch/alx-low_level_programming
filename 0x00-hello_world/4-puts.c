0e736ab9602b94ed9d03b7a80ae9c0ef8da5c787
Omar-driouch <omardriouch29@gmail.com>
2023-07-13 06:05:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,14 @@
 
 
 /**
- * main function is the entry point.
- * Return: 0 if exited properly.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-
-int main(void){
+int main(void)
+{
 
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }

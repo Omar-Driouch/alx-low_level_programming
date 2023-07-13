495574bef75b6686f686a80958b8568337f9cb52
Omar-driouch <omardriouch29@gmail.com>
2023-07-13 06:01:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * The main.c is the entrey point.
+ *
+ * the program returns 0 means exited with success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}

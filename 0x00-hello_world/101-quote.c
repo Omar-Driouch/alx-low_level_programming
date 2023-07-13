b20585450a1e7569b84d8e636f246f7d3d2e016d
Omar-driouch <omardriouch29@gmail.com>
2023-07-13 06:37:58 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - uses the write function to display the content.
+ * return 1 if exited .
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return 1;
+}

4bf512c46c46c70ee4347afc5b2559ec1a5c1c95
Omar-driouch <omardriouch29@gmail.com>
2023-07-13 06:23:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
-/*
- *
+/**
  *  main - prints the size of various types
  *  Program: Size of Various Types
  *  Description: This program prints the size of various types on the computer it is compiled and run on.

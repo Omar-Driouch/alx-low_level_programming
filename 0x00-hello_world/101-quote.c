a0c0828eac9ba608e0732d33ae2128345c9baac9
Omar-driouch <omardriouch29@gmail.com>
2023-07-13 06:43:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 
 /**
  * main - uses the write function to display the content.
- * return 1 if exited .
+ * Return: 1 to indicate an error condition as requested
+ * write(2, ...) will write the string directly to the standard error stream.
 */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return 1;
+	return (1);
 }

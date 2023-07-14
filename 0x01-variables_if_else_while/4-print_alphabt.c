e2852bb8c42d8f486856dfef64312d4b7f733d8a
Omar-driouch <omardriouch29@gmail.com>
2023-07-14 04:50:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets.
+ * Return: always 0 (Success).
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	do {
+		if(n == 113 || n == 101) {n++; }
+		else {
+			putchar (n);
+			n++;
+		}
+	} while (n < 123);
+	putchar ('\n');
+	return (0);
+}

bec1eab09a81084cdf8eba7538d58d044c1976ad
Omar-driouch <omardriouch29@gmail.com>
2023-07-14 07:51:38 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+		if (n >= 10) {
+			putchar(n - 10 + 'a');
+		} else {
+			putchar(n + '0');
+		}
+	putchar('\n');
+	return (0);
+}

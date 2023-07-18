6070e1f77f1a237010104317e6b36574359e97f8
Omar-driouch <omardriouch29@gmail.com>
2023-07-18 05:55:36 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day.
+ *
+ * Description: This function prints every minute of a fictional day, from 00:00 to 23:59.
+ *
+ * Return: void (no return value)
+ */
+
+void times_table(void)
+{
+	int hours = 0;
+
+	while (hours < 24)
+	{
+		int min = 0;
+
+		while (min < 60)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+			_putchar('\n');
+			min++;
+		}
+		hours++;
+	}
+}

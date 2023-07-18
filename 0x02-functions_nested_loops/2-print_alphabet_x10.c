5a1730e8fc2bb00623f3715d9697e44221f2bfe6
Omar-driouch <omardriouch29@gmail.com>
2023-07-18 03:18:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+        int i = 0 , j = 97;
+
+	while (i < 10)
+	{
+		while (j <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}

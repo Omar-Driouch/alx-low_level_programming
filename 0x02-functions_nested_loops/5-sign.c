5e62058d7c40c945029bd07d24592342873dbc8c
Omar-driouch <omardriouch29@gmail.com>
2023-07-18 04:47:29 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the sign fo the integer
+ *
+ * Return: 1 if n is positiv and -1 if n is negative and 0 if n equals to 0
+ */
+
+int print_sign(int n)
+{
+	if (n >= 1 )
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

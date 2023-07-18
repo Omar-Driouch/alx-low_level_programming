cac8b2aeca5899d5f05fa8acded1dd224b90621d
Omar-driouch <omardriouch29@gmail.com>
2023-07-18 04:37:50 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for lowercase and uppercase alphabetic
  * @c: c is an ascii character
  *
- * Return: 1 if c is alphabetic 
+ * Return: 1 if c is alphabetic
  */
 
 int _isalpha(int c)

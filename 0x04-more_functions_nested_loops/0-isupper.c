43fd5f235fa3e3afbe9f303e526ea92600e83dbc
Omar-driouch <omardriouch29@gmail.com>
2023-07-20 03:17:01 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,19 +5,11 @@
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
+
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-
-	return (isupper);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }

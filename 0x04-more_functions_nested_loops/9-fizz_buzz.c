30743fb23ba06cd884f4d3fc8fbddece3dd78294
Omar-driouch <omardriouch29@gmail.com>
2023-07-20 04:43:37 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100 with specific conditions.
+ *
+ * This function prints the numbers from 1 to 100. For multiples of three,
+ * it prints "Fizz" instead of the number. For multiples of five, it prints
+ * "Buzz" instead of the number. For numbers which are multiples of both
+ * three and five, it prints "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	return (0);
+}

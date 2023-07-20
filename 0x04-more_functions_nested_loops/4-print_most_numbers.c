55056d7846d00f39b207146adb6c5a4b5a45e833
Omar-driouch <omardriouch29@gmail.com>
2023-07-20 02:08:05 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != 2 && i != 4)
 		{
 			_putchar('0' + i);
 		}

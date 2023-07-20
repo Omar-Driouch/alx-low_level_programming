ae5f406562253243782f759fd5c6fc7a0f3a21d4
Omar-driouch <omardriouch29@gmail.com>
2023-07-20 06:44:23 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ *
+ * Return : void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else if (n < 100)
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if (n < 1000)
+	{
+		_putchar('0' + (n / 100));
+		n = n %  100;
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if (n < 10000)
+	{
+		_putchar('0' + (n / 1000));
+		n = n %  1000;
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	
+}

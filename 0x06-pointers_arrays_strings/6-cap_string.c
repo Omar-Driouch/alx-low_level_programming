1654b1c91ab0a5fea8b2b47cb93f4fd9bb01ee52
Omar-driouch <omardriouch29@gmail.com>
2023-07-26 06:49:56 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes the first letter of each word in a string.
+ * @s: Input string.
+ * Return: The pointer to the destination string.
+ */
+
+char *cap_string(char *c)
+{
+	const char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i , j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			for (j = 0 ; sep[j] != '\0' ; j++)
+			{
+				if(c[i-1] == sep[j])
+					c[i] = c[i] - 32;
+			}
+		}
+	}
+
+	return (c);
+}

d076f8cc81547c761022935c1ef097d4a9706b27
Omar-driouch <omardriouch29@gmail.com>
2023-07-26 11:40:23 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,7 @@
  * @dest: Pointer to the destination string.
  * @src: Pointer to the source string.
  * @n: Maximum number of characters to copy from 'src'.
- * Returns a pointer to the destination string (`dest`).
+ * Returns: a pointer to the destination string (`dest`).
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -14,8 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
+
 	for (; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
+
 	return (dest);
 }

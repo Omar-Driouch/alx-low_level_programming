1847cf3a92790605db30646db2d60078d1042286
Omar-driouch <omardriouch29@gmail.com>
2023-07-31 05:51:37 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+
+#endif /* MAIN_H */

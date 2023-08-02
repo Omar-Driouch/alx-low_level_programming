fa11398209990ede6448b8b328723b4a7e0fc71f
Omar-driouch <omardriouch29@gmail.com>
2023-08-02 03:05:17 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	write(1, s, 1);
-	_puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
 }

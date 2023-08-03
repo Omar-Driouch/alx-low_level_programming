e909a2b535ff80c198754b1febba8dee2bc23d4c
omar-driouch <omardriouch29@gmail.com>
2023-08-03 08:08:40 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,9 +9,6 @@
 
 int wildcmp(char *s1, char *s2)
 {
-
-	if (*s1 != '\0' && s2 == '*')
-		return (1);
 	if (*s1 == '\0' || *s2 == '\0')
 	{
 		if ((*s1 + '0') - (*s2 + '0') == 0)
@@ -26,5 +23,5 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
-	return (wildcmp(s1+1, s2+1));
+	return (wildcmp(s1 + 1, s2 + 1));
 }

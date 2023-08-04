21979a00a7261d827729579e7bab5b75c854d840
omar-driouch <omardriouch29@gmail.com>
2023-08-04 06:24:48 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (!isdigit(argv[i][j]) && (j == 0 && argv[i][j] != '-'))
+				if (!isdigit(argv[i][j]))
 				{
 					printf("Error\n");
 					return (1);

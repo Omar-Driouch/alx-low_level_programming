66d7c14995f33e2732f4b25919932380b32d17f8
omar-driouch <omardriouch29@gmail.com>
2023-08-04 05:41:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,10 +15,7 @@ int main(int argc, char *argv[])
 		printf("error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
 
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }

23e0f5d53cb72d7d749844caae5a1615c2b2bd17
omar-driouch <omardriouch29@gmail.com>
2023-08-04 04:10:27 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - is the entry point to print the programs name 
+ * main - is the entry point to print the programs name
  * @argc: the lenght of the argv array
- * @argv: an array of char 
+ * @argv: an array of char
  * Return: 0 exit with seccuess
  */
-
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf ("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }

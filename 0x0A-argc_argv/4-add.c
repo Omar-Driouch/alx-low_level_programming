e6657928f64c97c48b2bb463d38eb821c3afae52
omar-driouch <omardriouch29@gmail.com>
2023-08-04 07:04:02 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,30 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, res = 0, isValid;
+	int i, res;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	
 	for (i = 1; i < argc; i++)
 	{
-		isValid = 1;
+		int j;
+
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
-				isValid = 0;
-				break;
+				printf("Error\n");
+				return (1);
 			}
 		}
-		if (!isValid || (j == 0 && argv[i][j] == '\0'))
-		{
-			printf("Error\n");
-			return (1);
-		}
-
 		res = res + atoi(argv[i]);
 	}
 

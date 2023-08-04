7d68f371b0e1fe0cb67f83708086987194a593ee
omar-driouch <omardriouch29@gmail.com>
2023-08-04 04:00:49 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point to print the programs name 
+ * @argc: the lenght of the argv array
+ * @argv: an array of char 
+ * Return: 0 exit with seccuess
+ */
+
+
+int main (int argc, char *argv[])
+{
+	printf ("This program was called with \"%s\".\n",argv[0]);
+	prinft ("\n");
+
+	return (0);
+}

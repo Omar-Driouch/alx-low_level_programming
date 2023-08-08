ee47956395efaad5d8336a426a510080be715779
omar-driouch <omardriouch29@gmail.com>
2023-08-08 07:42:04 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,10 +21,10 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 
-	for (i = 0; i <= ac; i++)
+	for (i = 0; i < ac; i++)
 		total_len += strlen(av[i]) + 1;
 
-	A = (char *)malloc(total_len * sizeof(char));
+	A = (char *)malloc(total_len * sizeof(char) + 1);
 	if (A == NULL)
 		return (NULL);
 

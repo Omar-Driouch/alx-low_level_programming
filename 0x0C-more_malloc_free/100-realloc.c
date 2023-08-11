22df274d1a61a5362eb00b4355c322434b0de20e
omar-driouch <omardriouch29@gmail.com>
2023-08-11 14:44:23 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newptr;
 
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	newptr = realloc(ptr, new_size);

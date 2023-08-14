db7786e2db3ed3a38427832cf01af3d2252c7d46
omar-driouch <omardriouch29@gmail.com>
2023-08-14 02:05:57 -0700

    adding betty coding style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Struct dog
  * @name: name
@@ -11,4 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

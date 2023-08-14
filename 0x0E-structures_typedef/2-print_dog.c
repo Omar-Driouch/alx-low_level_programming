3b2078435f90130490af57b45fe983ea2d4dc692
omar-driouch <omardriouch29@gmail.com>
2023-08-14 02:32:34 -0700

    2-print_dog.c  a fucntion that prints the content of dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,5 +14,5 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
 	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+	printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nill)");
 }

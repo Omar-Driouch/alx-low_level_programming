f17c31baabc7043474144aa49c5178640256ceb2
Omar-Driouch <omardriouch29@gmail.com>
2023-08-16 23:23:52 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,10 +7,11 @@
  * @f:pointer to the printing function
  * Return: void.
  */
-    
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 	(*f)(name);
 }

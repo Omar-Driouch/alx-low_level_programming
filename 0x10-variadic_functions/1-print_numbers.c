99818884747acff953c7eef0ebb6d23b5ec3f82d
Omar-Driouch <omardriouch29@gmail.com>
2023-08-17 11:41:58 +0100

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	unsigned int i;
-	int s;
+	int current_number;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, int);
-		printf("%d", s);
-		if (i != n - 1)
+		if (i > 0)
 		{
 			printf("%s", separator);
 		}
+		current_number = va_arg(args, int);
+		printf("%d", current_number);
 	}
 	va_end(args);
 	putchar('\n');

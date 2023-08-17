a049c281f2bdfff51de811bfbb018a6109db16f0
Omar-Driouch <omardriouch29@gmail.com>
2023-08-17 12:46:40 +0100

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,13 @@
 #include "variadic_functions.h"
 
 /**
- * print_all -  a function that prints anything.
- * @format:  is a list of types of arguments passed to the function.
- *
+ * print_all - a function that prints anything.
+ * @format: a list of types of arguments passed to the function.
  * Return: void.
  */
-
 void print_all(const char *const format, ...)
 {
 	va_list args;
-	
-
 	const char *ptr = format;
 	int char_count = 0;
 
@@ -21,45 +17,34 @@ void print_all(const char *const format, ...)
 		switch (*ptr)
 		{
 		case 'c':
-		{
-			char c = va_arg(args, int);
-			printf("%c, ", c);
-			char_count++;
+			printf("%c", va_arg(args, int));
 			break;
-		}
 		case 'i':
-		{
-			int num = va_arg(args, int);
-			printf("%d, ", num);
-			char_count++;
+			printf("%d", va_arg(args, int));
 			break;
-		}
 		case 'f':
-		{
-			double num = va_arg(args, double);
-			printf("%f, ", num);
-			char_count++;
+			printf("%f", va_arg(args, double));
 			break;
-		}
 		case 's':
 		{
 			char *str = va_arg(args, char *);
+
 			if (str == NULL)
-			{
 				printf("(nil)");
-			}
 			else
-			{
-				printf("%s, ", str);
-				char_count++;
-			}
+				printf("%s", str);
 			break;
 		}
+		default:
+			break;
 		}
+
+		if (*(ptr + 1) != '\0' && (ptr[0] == 'c' || ptr[0] == 'i' ||
+								   ptr[0] == 'f' || ptr[0] == 's'))
+			printf(", ");
+
 		ptr++;
 	}
-
 	va_end(args);
-
 	printf("\n");
 }

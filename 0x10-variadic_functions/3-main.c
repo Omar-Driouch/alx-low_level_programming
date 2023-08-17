69a9fe39687699412662e3dadb323b0e9a92e11e
Omar-Driouch <omardriouch29@gmail.com>
2023-08-17 17:03:03 +0100

    3-print_all.c

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_all("ceis", 'B', 3, "stSchool");
+    print_all("ceis", 'B', 23, "stSchool");
     return (0);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,42 @@
 #include "variadic_functions.h"
+
 /**
-  * print_all - function that prints anything
-  * @format: list of types of arguments passed to the function
-  */
-void print_all(const char * const format, ...)
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char *const format, ...)
 {
-	int i = 0;
-	char *str, *sp = "";
-	const char *ptr = format;
-	va_list ls;
+    const char *ptr = format;
+    va_list ls;
 
-	va_start(ls, format);
+    va_start(ls, format);
 
-	if (*ptr)
-	{
-		while (format[i])
-		{
-			switch (*ptr)
-			{
-				case 'c':
-					printf("%s%c", sp, va_arg(ls, int));
-					break;
-				case 'i':
-					printf("%s%d", sp, va_arg(ls, int));
-					break;
-				case 'f':
-					printf("%s%f", sp, va_arg(ls, double));
-					break;
-				case 's':
-					str = va_arg(ls, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sp, str);
-					break;
-				default:
-					i++;
-					ptr++;
-					continue;
-			}
-			sp = ", ";
-			i++;
-			ptr++;
-		}
-	}
-	printf("\n");
-	va_end(ls);
+    while (*ptr != '\0')
+    {
+        switch (*ptr)
+        {
+        case 'i':
+            printf("%d", va_arg(ls, int));
+            break;
+        case 'f':
+            printf("%f", va_arg(ls, double));
+            break;
+        case 'c':
+            printf("%c", va_arg(ls, int));  
+            break;
+        case 's':
+            printf("%s", va_arg(ls, char *));
+            break;
+        default:
+            break;
+        }
+
+        if (*(ptr + 1) != '\0')
+            printf(", ");
+
+        ptr++;
+    }
+
+    printf("\n");
+    va_end(ls);
 }

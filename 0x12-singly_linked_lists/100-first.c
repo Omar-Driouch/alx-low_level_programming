de02a6dd8a9d1402ef33f24366c89522c9399386
Omar-Driouch <omardriouch29@gmail.com>
2023-08-24 14:13:54 +0100

    function get executed  before the main function is does

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__((constructor)) before_main(void)
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }

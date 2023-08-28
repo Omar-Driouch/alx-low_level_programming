f0b8fefcdcf1f36cb8952aed8d99c46fcbe0573a
Omar-Driouch <omardriouch29@gmail.com>
2023-08-28 17:55:02 +0100

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (current != NULL)
 	{
-		if (i == index - 1)
+		if (i == index)
 		{
 			current = *head;
 			*head = current->next;

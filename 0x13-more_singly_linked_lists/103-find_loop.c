fcde3148277fc76cadb97c8542a6932c48830c2c
Omar-Driouch <omardriouch29@gmail.com>
2023-08-29 13:51:07 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -4,26 +4,26 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: pointer to the head of the list
  *
- * Return: The address of the node where the loop starts, or NULL if there is no loop
+ * Return: The address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *L = head;
+	listint_t *l = head;
 	listint_t *h = head;
 
-	while (L && h && h->next)
+	while (l && h && h->next)
 	{
-		L = L->next;
+		l = l->next;
 		h = h->next->next;
-		if (L == h)
+		if (l == h)
 		{
-			L = head;
-			while (L != h)
+			l = head;
+			while (l != h)
 			{
-				L = L->next;
+				L = l->next;
 				h = h->next;
 			}
-			return L;
+			return (l);
 		}
 	}
 

83f31800c74c109442b5f32034bd2735e6d6295b
Omar-Driouch <omardriouch29@gmail.com>
2023-09-04 15:08:02 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,25 +10,21 @@
 ssize_t read_textfile(const char *filename, size_t num_letters)
 {
 	int file_descriptor;
-	ssize_t num_bytes_read, num_bytes_written;
+	ssize_t num_bytes_read, bytes_written;
 	char *buffer;
 
 	if (!filename)
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
-	
 	if (file_descriptor == -1)
 		return (0);
-
 	buffer = malloc(num_letters);
-
 	if (!buffer)
 	{
 		close(file_descriptor);
 		return (0);
 	}
-
 	num_bytes_read = read(file_descriptor, buffer, num_letters);
 
 	if (num_bytes_read == -1)
@@ -37,18 +33,14 @@ ssize_t read_textfile(const char *filename, size_t num_letters)
 		close(file_descriptor);
 		return (0);
 	}
-
-	num_bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
-
-	if (num_bytes_written == -1 || (size_t)num_bytes_written != (size_t)num_bytes_read)
+	bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)num_bytes_read)
 	{
 		free(buffer);
 		close(file_descriptor);
 		return (0);
 	}
-
 	close(file_descriptor);
 	free(buffer);
-
-	return (num_bytes_written);
+	return (bytes_written);
 }

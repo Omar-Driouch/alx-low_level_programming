cbf619fd2f1102ae2b5eaf487874815cbd344035
Omar-Driouch <omardriouch29@gmail.com>
2023-09-04 16:34:39 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		return (-1);
-	}
 
 	check = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writ = write(check, text_content, strlen(text_content));

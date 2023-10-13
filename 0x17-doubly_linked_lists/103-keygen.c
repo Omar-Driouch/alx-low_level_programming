414b10caebdb35dbe89a0c9a1ef3a96b4d186617
Omar-Driouch <omardriouch29@gmail.com>
2023-10-14 00:00:50 +0100

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,12 +3,12 @@
 
 /**
  * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
- *
+
  * Return: 0 on success, 1 on error
  */
-int main(int argc, char *argv[])
+int main(void)
 {
+
+
 	return (0);
 }
